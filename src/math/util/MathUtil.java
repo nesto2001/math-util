016bdef602fbee192788d82b4a690aa2e41f426a
nesto2001 <tanlong6121@gmail.com>
2021-03-14 16:25:24 +0700

    1st upload code to Github

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ */
+package math.util;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello GitHub!!!");
+    }
+    
+}
